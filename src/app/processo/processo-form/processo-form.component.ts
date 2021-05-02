fe4b27668b9b2836686e4c34f337176176a50afb
Daniel Ferreira <danenoisturbera@gmail.com>
2021-05-02 16:36:58 -0300

    finalizando html e form control do formulario

--- src/app/processo/processo-form/processo-form.component.ts
+++ src/app/processo/processo-form/processo-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Processo } from '../shared/processo';
 
 @Component({
   selector: 'app-processo-form',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProcessoFormComponent implements OnInit {
 
-  constructor() { }
+  processoForm: FormGroup;
+  processoSelecionado: Processo = new Processo();;
+  constructor(private fb: FormBuilder ) {
+    this.criarForm();
+   }
 
   ngOnInit(): void {
+    
+  }
+
+  criarForm(){
+    this.processoForm = this.fb.group({
+      numeroProcesso: ['', [
+        Validators.required,
+        Validators.minLength(12),
+        Validators.maxLength(12),
+        Validators.pattern("^[0-9]*$")
+      ]],
+      valor: ['', [
+        Validators.required,
+        Validators.min(30000)
+      ]],
+      escritorio: ['', [
+        Validators.required,
+        Validators.minLength(3)]
+      ],
+      reclamante: ['', [
+        Validators.required]],
+      aprovado: [false, [
+      ]],
+      ativo: [false, [
+      ]]
+
+    });
+  }
+
+  salvar(){
+    console.log('enviou')
   }
 
 }
